eb63e55e902cab735bca587e0965110a153e0675
Michael Kness <mpkness@gmail.com>
2025-03-03 22:17:23 -0800

    fix(): will pull full articles for each of the links, right now only pulls from p tag

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # news-topic-search
 Give a news topic and scrape available news sites to get relevant articles
+
+## how to use
+run ` python src/main.py --topic test --to_file true --debug true` in console
+
+### options
+
+- --topic <string> the topic to search for
+- --debug <bool> will write a debug log
+- --to_file <bool> will write the output of the script to file
+
+github repo with a big list of rss feeds: https://github.com/plenaryapp/awesome-rss-feeds?tab=readme-ov-file#News
--- src/main.py
+++ src/main.py
@@ -6,8 +6,14 @@ from urllib.parse import urlparse
 import argparse
 import json
 
-def main(topic=None):
-    listOfAPIs = [ # TODO: most these apis needs some kinds of setup to connect to
+# Define a global variable for debug mode
+DEBUG_MODE = False
+
+def main(topic=None, debug=False):
+    global DEBUG_MODE
+    DEBUG_MODE = args.debug
+
+    listOfAPIs = [ # TODO: most these apis need some kinds of setup to connect to
         "https://newsapi.org/v2/top-headlines",
         "https://gnews.io/api/v4/top-headlines",
         "http://api.mediastack.com/v1/news",
@@ -26,26 +32,41 @@ def main(topic=None):
 
     # Dictionary mapping of websites to where they keep their articles
     site_tags = {
-        "bbc.co.uk": {"data-component": "text-block"},
+        "bbc.co.uk": {"tag-type": "p"},
     }
 
     if topic is None:
         topic = input("Enter the topic you want to search for: ")
 
     filtered_entries, numberOfArticles = filter_rss_feeds(listOfRssFeeds, topic)
-    entries_info = [
-        {
-            "title": entry.title,
-            "link": entry.link,
-            "published": entry.published,
-            "source": entry.source.title if hasattr(entry, 'source') else None
-        } for entry in filtered_entries]
-
     if filtered_entries:
-        first_entry_url = filtered_entries[1].link
-        base_url = urlparse(first_entry_url).netloc
-        tag = site_tags.get(base_url, {"data-component": "article-body"})  # Default to 'article-body' if the site is not in the dictionary
-        full_article = fetch_full_article(first_entry_url, tag)
+        entries_info = []
+        for entry in filtered_entries:
+            entry_url = entry.link
+            base_url = urlparse(entry_url).netloc
+            tagType = site_tags.get(base_url, {"tag-type": "p"})  # Default to 'p' if the site is not in the dictionary
+            full_article = fetch_full_article_by_tag(entry_url, tagType['tag-type'])
+
+            #if DEBUG_MODE:
+            #    with open('debug.log', 'a') as debug_file:
+            #        debug_file.write(f"Full article:\n{full_article}\n")
+            
+            entry_info = {
+                "title": entry.title,
+                "link": entry.link,
+                "published": entry.published,
+                "source": entry.source.title if hasattr(entry, 'source') else None,
+                "base_url": base_url,
+                "full_article": full_article
+            }
+            entries_info.append(entry_info)
+
+        if DEBUG_MODE:
+            with open('debug.log', 'w') as debug_file:
+               debug_file.write(f"Filtered entries: {json.dumps(entries_info, indent=2)}\n")    
+    else:
+        entries_info = []
+
 
     return entries_info, numberOfArticles
 
@@ -61,12 +82,13 @@ def filter_rss_feeds(feeds, topic):
                 filtered_entries.append(entry)
     return filtered_entries, numberOfEntries
 
-def fetch_full_article(url, tag):
+def fetch_full_article_by_tag(url, tag):
+    print(f"Fetching full article from {url} using tag {tag}...")
     response = requests.get(url)
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'html.parser')
         # Extract text based on the provided tag
-        elements = soup.find_all('p')
+        elements = soup.find_all(tag)
         article_text = '\n'.join([element.get_text() for element in elements])
         return article_text
     else:
@@ -75,7 +97,14 @@ def fetch_full_article(url, tag):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Search for news articles by topic.")
     parser.add_argument('--topic', type=str, help='The topic to search for')
+    parser.add_argument('--to_file', type=bool, help='Save the output to a file')
+    parser.add_argument('--debug', type=bool, help='Add extra debug information')
     args = parser.parse_args()
 
-    entries_info, numberOfArticles = main(args.topic)
-    print(json.dumps(entries_info, indent=2))
+    entries_info, numberOfArticles = main(args.topic, args.debug)
+
+    if args.to_file:
+        with open('news_articles.json', 'w') as file:
+            json.dump(entries_info, file, indent=2)
+    else:
+        print(json.dumps(entries_info, indent=2))
